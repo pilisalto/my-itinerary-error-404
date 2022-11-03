0bf5b385caa0d4663052b2b1349323356780309f
juan-francoa <esteban_rlt159@hotmail.com>
2022-11-03 09:31:35 -0500

    creacion 3 primeros archivos

---
+++ src/data/citys.js
@@ -0,0 +1,101 @@
+let citys = [
+    {
+        id:"city1",
+        name:"Bello",
+        continent:"America del Sur",
+        photo:"https://www.bing.com/th?id=OSK.HERO_GCPmdX1I3VRqg-N5Av2c1FYM7DR_qM3vPJhXxwrePE&w=296&h=176&c=1&rs=2&o=6&dpr=1.5&pid=SANGAM",
+        population:522264,
+        userId:"admin1"   
+    },
+    {
+        id:"city2",
+        name:"Rosario",
+        continent:"America del Sur",
+        photo:"https://images.clarin.com/2015/11/10/BksaAQJVe_1256x620.jpg",
+        population:1199364,
+        userId:"admin2" 
+    },
+    {
+        id:"city3",
+        name:"Cordoba",
+        continent:"America del Sur",
+        photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Skyline_of_downtown_Cordoba_city%2C_Argentina_2013-02-24.jpg/600px-Skyline_of_downtown_Cordoba_city%2C_Argentina_2013-02-24.jpg",
+        population:1655481,
+        userId:"admin3" 
+    },
+    {
+        id:"city4",
+        name:"New york",
+        continent:"America del Norte",
+        photo:"https://www.aviatur.com/source/contenidos/blog/nueva-york-lugares-turisticos-capital-del-mundo.jpg",
+        population:8500000,
+        userId:"admin4" 
+    },
+    {
+        id:"city5",
+        name:"Cancún",
+        continent:"America Central",
+        photo:"https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_650,q_75,w_1200/v1/clients/quintanaroo/atardecer_en_canc_n_t20_4eyeZA_51e7530b-ecbb-4a93-a51b-b238cb5d898e.jpg",
+        population:888797,
+        userId:"admin1" 
+    },
+    {
+        id:"city6",
+        name:"Bariloche",
+        continent:"America del Sur",
+        photo:"https://denomades.s3.us-west-2.amazonaws.com/blog/wp-content/uploads/2020/05/08213126/shutterstock_1371228326.jpg",
+        population:2146000,
+        userId:"admin2" 
+    },
+    {
+        id:"city7",
+        name:"Amsterdam",
+        continent:"Europa",
+        photo:"https://media.timeout.com/images/105504583/1024/576/image.jpg",
+        population:5905234,
+        userId:"admin3" 
+    },
+    {
+        id:"city8",
+        name:"Barcelona",
+        continent:"Europa",
+        photo:"https://media.timeout.com/images/105504583/1024/576/image.jpg",
+        population:5714730,
+        userId:"admin4" 
+    },
+    {
+        id:"city9",
+        name:"Londres",
+        continent:"Europa",
+        photo:"https://londresen3dias.com/wp-content/uploads/big-ben_2-1.jpg",
+        population:10470000,
+        userId:"admin1" 
+    },
+    {
+        id:"city10",
+        name:"Sidney",
+        continent:"Oceania",
+        photo:"https://i0.wp.com/viajealpatrimonio.com/wp-content/uploads/2019/11/0697-opera-de-sidney.jpg?ssl=1",
+        population:4119190,
+        userId:"admin2" 
+    },
+    {
+        id:"city11",
+        name:"Paris",
+        continent:"Europa",
+        photo:"http://3.bp.blogspot.com/--uxk6Z7gXZg/UQgWarMa9II/AAAAAAAAk-U/CnQ1jY6blGM/s1600/Paris_at_sunset_with_the_Eiffel_Tower.jpg",
+        population:2273250,
+        userId:"admin3" 
+    },
+    {
+        id:"city12",
+        name:"Roma",
+        continent:"Europa",
+        photo:"https://www.raffaeleangelillo.com/wp-content/uploads/2019/09/Roma.jpg",
+        population:2765000,
+        userId:"admin4" 
+    }
+    
+]
+
+export default citys

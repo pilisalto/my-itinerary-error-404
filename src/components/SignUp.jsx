abf8746a415e827c769caec38800a18fa3876caf
Pilar Salto <m.pilarsalto@gmail.com>
2022-11-07 09:42:03 -0300

    singup

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import users from '../data/user'
+import NavBar from './NavBar'
+
+export default function SignUp() {
+    let [emaili, setEmaili] = useState("")
+    const event = (e) => {
+        setEmaili(e)
+    }
+    const confEmail = (a) => {
+        
+        let bool = false
+        let array = []
+        for(let i=0;i<5;i++){
+            if(a.target.form[i].value.length){
+                array.push(a.target.form[i].value)
+                bool = true
+            }
+            else{
+                bool = false
+                return bool
+            }
+        } 
+        if(bool){
+            let user = users.find(e => e.email === emaili.toLowerCase())
+            if(user){
+                alert("Correo ya registrado")
+            }
+            else{
+                localStorage.setItem("user",array)
+                alert("Registro exitoso")
+            }
+        }
+        console.log(bool)
+
+    }
+    return (
+        <>
+        <div className='image_back2'>
+        <div className='home3'>
+            <div><NavBar/></div>
+                <h1 className='h1 row'>Registrati<span className='resaltado'>on</span></h1>
+                <div className='container1'>
+                    <form>
+                        <div>
+                            <div>
+                                <label className='.titulo'>Full Name</label>
+                                <input className='input' type="text" placeholder="Enter your name " required />
+                            </div>
+                            <div >
+                                <label >Username</label>
+                                <input  className='input' type="text" placeholder="Enter your Username " required />
+                            </div>
+                            <div>
+                                <label>Contact No</label>
+                                <input className='input' type="text" placeholder="Enter your Contact No " required />
+                            </div>
+                            <div>
+                                <label>E-mail</label>
+                                <input className='input' type="text" onChange={e => event(e.target.value)} placeholder="Enter your Email " required />
+                            </div>
+                            <div>
+                                <label>Password</label>
+                                <input className='input' type="password" placeholder="Enter your Password " required />
+                            </div>
+                        </div>
+                        <div>
+                            <input onClick={a =>confEmail(a)} type="submit" className='boton1 a send1' value="Register" />
+                        </div>
+                        </form>
+                <Link to={"/signin"} className='none a'>Do you have An account?</Link>
+        </div>
+        </div>
+        </div>
+        </>
+    )
+}

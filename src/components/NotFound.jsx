75b2d8f212b39acd46da91b2317f69e4fabe2df8
Pilar Salto <m.pilarsalto@gmail.com>
2022-11-07 09:32:35 -0300

    agrego NotFound

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CallToAction from './CallToAction'
+
+export default function NotFound() {
+  return (
+    <div><div>NotFound</div>
+            <CallToAction link={"/"} text={"Back Home"}/> 
+    </div>
+  )
+}

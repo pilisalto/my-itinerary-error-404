84d3a2ef16c0416e5078a563a858230635cce664
Pilar Salto <m.pilarsalto@gmail.com>
2022-11-07 10:15:53 -0300

    agrego shows, contacto, componentes, imagenes y css

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import AutoToTop from './AutoToTop'
+import Cities from './Cities'
+import City from './City'
+import Hotels from "./Hotels"
 import Home from './Home'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+import Hotel from "./Hotel"
+import Contact from './Contact'
+import Shows from './Shows'
+import Itinerary from './Itinerary'
+
 
 
 export default function Redirect() {
@@ -10,6 +20,15 @@ export default function Redirect() {
     <AutoToTop/>
       <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/cities' element={<Cities/>}/>
+            <Route path='/hotels' element={<Hotels/>} />
+            <Route path='/itinerary' element={<Itinerary/>}/>
+            <Route path='/shows' element={<Shows/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/signup' element={<SignUp/>}/>
+            <Route path='/signin'element={<SignIn/>} />
+            <Route path='/city/:setIndex'element={<City/>} />
+            <Route path='/hotel/:setHotel'element={<Hotel/>} />
       </Routes> 
     </>
   )

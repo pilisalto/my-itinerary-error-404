e6a3927d96c408ccbe540493cffa2ebea59c6e4e
juan-francoa <esteban_rlt159@hotmail.com>
2022-11-13 20:57:09 -0500

    conectando back con front para cities

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -1,94 +1,76 @@
 import React from 'react'
-import citys from '../data/citys'
 import CityCard from './CityCard'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
-import appli from '../data/appli'
 import NavBar from './NavBar'
+import { BASE_URL } from '../api/url'
+import axios from 'axios'
+import { useEffect } from 'react'
+
+export default function Cities() {  
+  let [filcheck, setFilcheck] = useState("")
+  let [inp, setInp] = useState("")
+  let [api, setApi] = useState([])
+  let [effect, setEffect] = useState()
+  let [ho, setHo] = useState()
+  
+  useEffect(() => {
+    setEffect(axios.get(`${BASE_URL}/api/cities`).then((res) => {
+      (setApi(res.data.response))
+    })
+      .catch(err => console.log(err)))
+      axios.get(`http://localhost:8000/api/cities/`)
+            .then(res => setHo(res.data.data))
+            .catch(err => console.log(err))
+  },[])
+
+  
+  let checkbox = ([... new Set(api.map(e => e.continent))].map(s => <form><label>{s}<input type="checkbox" onClick={e => check(e.target.value)} value={s} ></input> </label></form >))
+  
 
 
-export default function Cities() {
-  let [cards, setCards] = useState(citys.map((e, b) => (<Link to={"/city/" + citys[b].id}><CityCard name={e.name} photo={e.photo} /></Link>)))
-  let [conti, setConti] = useState([])
-  let [cardSearch, setCardSearch] = useState(citys)
-  let [cardIm, setCardIm] = useState(citys)
-  let [array, setArray] = useState(citys)
-  let checkbox = ([... new Set(citys.map(e => e.continent))].map(s => <form><label>{s}<input type="checkbox" onClick={e => filterCru("filterCheck",e.target.value)} value={s} ></input> </label></form >))
-  const filterCru = (fn, value) => {
-    setArray(array = citys)
-    if (fn === "filterCheck") {
-      if (!conti.includes(value)) {
-        conti.push(value)
+  function check(e) {
+    if (filcheck.includes(e)) {
+      
+      setFilcheck(filcheck.replace(e, ""))
+    }
+    else {
+      if (filcheck.length > 0) {
+        setFilcheck(filcheck = filcheck + "," + e)
       }
       else {
-        let i = conti.indexOf(value)
-        conti.splice(i, 1)
+        setFilcheck(e)
       }
-      appli[fn] = conti
-    }
-    else {
-      appli[fn] = value
-    }
-    console.log(appli)
-    for (let name in appli) {
-      if (name === "cardFilter") {
-        setArray(array = cardFilter(array, appli[name]))
-      }
-      if (name === "filterCheck") {
-        setArray(array = filterCheck(array, appli[name]))
-      }
-    }
-    setArray(array)
-    console.log(array)
-    localStorage.setItem("cities",JSON.stringify(array))
-    return array
+    }  
+    setEffect(axios.get(`${BASE_URL}/api/cities?name=${inp}&continent=${filcheck}`).then((response) => {
+      setApi(response.data.response)
+    })
+      .catch(err => console.log(err)))   
   }
-  const filterCheck = (ci,e) => {
-    if (e.length > 0) {
-      let gh = ci.filter(d => e.includes(d.continent))
-      if (gh.length) {
-        setCardIm(cardIm = gh)
-        return gh
-      }
-      else {
-        setCardIm(cardIm = ci)
-        return ci
-      }
-    }
-    else {
-      setCardIm(cardIm = ci)
-      return ci
-    }
-
+  function funInput(e) {
+    setInp(e)
+    setEffect(axios.get(`${BASE_URL}/api/cities?name=${e}&continent=${filcheck}`).then((response) => {
+      setApi(response.data.response)
+    })
+      .catch(err => console.log(err)))
   }
-
-  const cardFilter = (ci, es) => {
-    if (es.length) {
-      let bh = ci.filter(i => i.name.toLowerCase().includes(es.toLowerCase()))
-      setCardSearch(cardSearch = bh)
-      return bh
-    }
-    else {
-      setCardSearch(cardSearch = ci)
-      return ci
-    }
-  }
-
+ console.log(inp, filcheck)
+  //console.log(api.response.map((e, b, c) => (<Link to={"/city/" + c[b]._id}><CityCard name={e.name} photo={e.photo} /></Link>)))
   //<CityCard name={e.name} photo={e.photo} continent={e.continent} population={e.population}/>
   return (
     <>
       <div className='image_back4'>
-        <div><NavBar/></div>
+        <div><NavBar /></div>
         <div className='home1'>
-        <h1 class="h1 row">Ci<span className='resaltado'>t</span>ies</h1>
-        <div className='contenedor'>
-          <div className='check'>{checkbox}</div>
-          <form role="search">
-            <input type="search" className='search' onChange={e => filterCru("cardFilter",e.target.value)} placeholder="Search" /> <img src="/img/icons/busqueda.png" alt="" />
-          </form>
+          <h1 class="h1 row">Ci<span className='resaltado'>t</span>ies</h1>
+          <div className='contenedor'>
+            <div className='check'>{checkbox}</div>
+            <form role="search">
+              <input type="search" className='search' onChange={e => funInput(e.target.value)} placeholder="Search" /> <img src="/img/icons/busqueda.png" alt="" />
+            </form>
+          </div>
+          <div className='cards_flex none'>{api.map((e, b, c) => (<Link to={"/city/" + c[b]._id}><CityCard name={e.name} photo={e.photo} /></Link>))}</div>
         </div>
-      <div className='cards_flex none'>{cards}</div>
-      </div>
       </div>
     </>
   )

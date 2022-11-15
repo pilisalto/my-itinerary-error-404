3b9bec2d766ab465030973c9ea02487ecd19a6f5
pilisalto <m.pilarsalto@gmail.com>
2022-11-15 00:19:26 -0300

    agrego newcity y new hotel

--- src/layouts/Layouts.jsx
+++ src/layouts/Layouts.jsx
@@ -5,7 +5,7 @@ import Footer from '../components/Footer'
 export default function Layouts() {
   return (
     <div className='flex direction-column align-center v100 h100'>
-      <div className='grow '><Redirect/></div>
+      <div className='grow'><Redirect/></div>
       <div className=''><Footer/></div>
     </div>
   )

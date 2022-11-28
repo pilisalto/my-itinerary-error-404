4d78e8cd4493f04756949d66105d69167dec08b7
juan-francoa <esteban_rlt159@hotmail.com>
2022-11-28 05:53:22 -0500

    MT-02-52

--- src/redux/reducer/myTinerariesReducer.js
+++ src/redux/reducer/myTinerariesReducer.js
@@ -10,7 +10,6 @@ const initialState= {
 const myTinerariesReducer = createReducer(initialState, (builder)=>{
   
     builder.addCase(myTinerariesAction.filtrarTineraries.fulfilled, (state, action)=>{
-        console.log(action.payload.tinerariesFiltrados)
         return {...state, loading: false, tinerariesFiltrados:action.payload.tinerariesFiltrados}
     })
     builder.addCase(myTinerariesAction.filtrarTineraries.rejected, (state, action)=>{

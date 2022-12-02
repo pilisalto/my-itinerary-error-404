0de020c8e854e54cdfb0368a5f2990ac4b84726d
juan-francoa <esteban_rlt159@hotmail.com>
2022-12-02 03:05:50 -0500

    MT-02-58

--- src/redux/actions/commentAction.js
+++ src/redux/actions/commentAction.js
@@ -10,36 +10,68 @@ let getComment = createAsyncThunk("getComment", async (data) => {
             response: res.data.response,
             message: res.data.message
         }
-    
+
     }
-    catch(error){
+    catch (error) {
         console.log(error)
-        return { 
+        return {
             success: false,
             response: [],
             message: error.response.data
         }
     }
 })
-const postComment = createAsyncThunk("postComment", async ( idCity ) => {
+const postComment = createAsyncThunk("postComment", async (idCity) => {
     let url = `${BASE_URL}/api/comments`;
-    let headers = {headers: {'Authorization': `Bearer ${idCity.token}`}}
+    let headers = { headers: { 'Authorization': `Bearer ${idCity.token}` } }
     try {
-      const res = await axios.post(url,idCity.data ,headers);
-      return {
-        success: true,
-      };
+        const res = await axios.post(url,idCity.data , headers);
+        return {
+            success: true,
+        };
     } catch (error) {
-      console.log(error);
-      return {
-        error: true,
-      };
+        console.log(error);
+        return {
+            error: true,
+        };
     }
-  });
-
+});
+const deletComment = createAsyncThunk("deletComment", async (idCity) => {
+    let url = `${BASE_URL}/api/comments/${idCity.id}`;
+    let headers = { headers: { 'Authorization': `Bearer ${idCity.token}` } }
+    try {
+        const res = await axios.delete(url, headers);
+        return {
+            success: true,
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            error: true,
+        };
+    }
+});
+const putComment = createAsyncThunk("putComment", async (idCity) => {
+    let url = `${BASE_URL}/api/comments/${idCity.id}`;
+    let headers = { headers: { 'Authorization': `Bearer ${idCity.token}` } }
+    try {
+        const res = await axios.put(url,{comment:idCity.comment} , headers);
+        console.log(res, idCity.comment)
+        return {
+            success: true,
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            error: true,
+        };
+    }
+});
 const commentAction = {
     getComment,
-    postComment
+    postComment,
+    deletComment,
+    putComment
 }
 
 export default commentAction;
